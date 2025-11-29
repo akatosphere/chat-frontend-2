75e6507fd6b9f3ae7ba9dc0d406d8330c04ab58a
oseniy <ars7080947@gmail.com>
2025-11-29 19:14:54 +0300

    feat: кнопки, шрифты и цвета
    в предыдущем коммите случайно не добавил некоторые файлы в staged

---
+++ src/shared/shadcn/lib/utils.ts
@@ -0,0 +1,25 @@
+import { type ClassValue, clsx } from "clsx"
+import { extendTailwindMerge } from "tailwind-merge"
+
+const customTwMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': [        
+        "headline",
+        "subheadline",
+        "title",
+        "text",
+        "text-tight",
+        "subtext",
+        "subtext-tight",
+        "minitext",
+        "minitext-tight",
+        "caption",
+        "caption-tight",], 
+    },
+  },
+})
+
+export function cn(...inputs: ClassValue[]) {
+  return customTwMerge(clsx(inputs))
+}
