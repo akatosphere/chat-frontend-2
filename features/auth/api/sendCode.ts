e7956d25f1ecca82249c3486fe2807c056bb512a
lilsantt <brusnicagv@gmail.com>
2025-12-05 23:12:23 +0300

    feat: хук useActions и скрипт createAction

--- features/auth/api/sendCode.ts
+++
@@ -1,9 +0,0 @@
-import api from "@/shared/api/api-client";
-
-export interface SendCodeData {
-  phone_number: string;
-}
-
-export const sendCode = (data: SendCodeData) => {
-  return api.post("/api/v1/auth/messenger/login/get/code/", data);
-};
