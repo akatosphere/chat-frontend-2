804387732f7ae657e506c79bf4db275c79c16e3a
oseniy <ars7080947@gmail.com>
2025-12-17 01:32:23 +0300

    feat: ресайз текстового поля

--- src/features/chat/sendMessage/ui/messageForm.tsx
+++ src/features/chat/sendMessage/ui/messageForm.tsx
@@ -1,20 +1,47 @@
-import { cn } from '@/shared/shadcn/lib/utils';
-import { InputGroupTextarea } from '@/shared/shadcn/ui/input-group';
+import { cn } from '@/shared/shadcn/lib/utils'
+import { InputGroupTextarea } from '@/shared/shadcn/ui/input-group'
+import { useLayoutEffect, useRef } from 'react'
 
-type MessageFormProps = {
-  className?: string,
-};
+type MessageFormProps = { className?: string }
 
-export const MessageForm : React.FC<MessageFormProps> = ({
-  className,
-}) => {
+export const MessageForm: React.FC<MessageFormProps> = ({ className }) => {
+  const ref = useRef<HTMLTextAreaElement | null>(null)
+
+  const resize = () => {
+    const el = ref.current
+    if (!el) return
+
+    el.style.height = 'auto'
+    el.style.height = `${el.scrollHeight}px`
+  }
+
+  useLayoutEffect(() => {
+    resize()
+  }, [])
+
   return (
-    <form className={cn('py-3 flex ', className)}>
-      <div className='flex-1'/>
-      <InputGroupTextarea placeholder='Сообщение' className="subtext flex-3 h-11 bg-white rounded-3xl [&::-webkit-scrollbar]:hidden">
-      
-      </InputGroupTextarea>
-      <div className='flex-1'/>
+    <form className={cn('py-3 flex', className)}>
+      <div className="flex-1" />
+
+      <InputGroupTextarea
+        ref={ref}
+        rows={1}
+        placeholder="Сообщение"
+        className="
+                    subtext
+                    flex-3
+                    h-11
+                    min-h-11
+                    max-h-[172px]
+                    overflow-y-auto
+                    resize-none
+                    bg-white
+                    rounded-3xl
+                    [&::-webkit-scrollbar]:hidden
+                "
+      />
+
+      <div className="flex-1" />
     </form>
   )
-};
+}
