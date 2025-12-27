8a57a438898410a09543ccce89953c32ec543f16
oseniy <ars7080947@gmail.com>
2025-12-27 12:36:36 +0300

    fix: убрал ненужный ресайз футера

--- src/features/chat/sendMessage/lib/helpers.ts
+++ src/features/chat/sendMessage/lib/helpers.ts
@@ -1,5 +1,9 @@
 export const resize: React.FormEventHandler<HTMLTextAreaElement> = (e) => {
   const el = e.currentTarget;
+  const style = window.getComputedStyle(el);
+
+  const padding = parseFloat(style.paddingTop) + parseFloat(style.paddingBottom);
+
   el.style.height = "auto";
-  el.style.height = `${el.scrollHeight}px`;
+  el.style.height = `${el.scrollHeight - padding}px`;
 };
