b5dfaf9fbb0dd5d034c3fbb43f583dd711ae41a9
oseniy <ars7080947@gmail.com>
2026-01-10 11:50:40 +0300

    feat: добавил второе поле

--- src/features/groups/createGroup/ui/nameDescriptionFields.tsx
+++ src/features/groups/createGroup/ui/nameDescriptionFields.tsx
@@ -9,15 +9,26 @@ type NameDescriptionFieldsProps = {
 export const NameDescriptionFields: React.FC<NameDescriptionFieldsProps> = ({ className }) => {
   return (
     <div className={cn("", className)}>
-      <InputGroup className="relative flex h-min w-full rounded-lg bg-white">
-        <div className="reletive desktop:max-h-[129px] flex max-h-[102px] flex-1 overflow-hidden rounded-3xl">
+      <InputGroup className="border-muted relative flex h-min w-full rounded-t-lg rounded-b-none border-b bg-white">
+        <div className="reletive desktop:max-h-[129px] flex max-h-[102px] flex-1 overflow-hidden">
           <InputGroupTextarea
             name="groupName"
-            className="subtext h-14 min-h-11 resize-none overflow-hidden py-4"
+            className="subtext h-14 min-h-14 resize-none overflow-hidden py-4.5"
             onInput={resizeTextarea}
             placeholder="Название*"
           />
         </div>
+      </InputGroup>
+      <InputGroup className="relative flex h-min w-full rounded-t-none rounded-b-lg bg-white">
+        <div className="reletive desktop:max-h-[129px] flex max-h-[102px] flex-1 overflow-hidden">
+          <InputGroupTextarea
+            name="groupDescription"
+            className="subtext h-14 min-h-14 resize-none overflow-hidden py-4.5"
+            onInput={resizeTextarea}
+            placeholder="Описание"
+          />
+        </div>
+        {/* делаю красивый ресайз полей*/}
       </InputGroup>
     </div>
   );
