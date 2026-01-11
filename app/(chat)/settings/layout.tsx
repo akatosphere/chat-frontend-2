6d73ca64a3a00c38e8255cbc799a2ac208ec6b59
oseniy <ars7080947@gmail.com>
2026-01-11 14:26:17 +0300

    test: создал тестовые кнопки для запроса

--- app/(chat)/settings/layout.tsx
+++ app/(chat)/settings/layout.tsx
@@ -1,22 +1,17 @@
 "use client";
-import { getSocket } from "@/shared/api/wsClient";
-import { Button } from "@/shared/shadcn/ui/button";
+import { QueryBtn } from "@/shared/test/queryBtn";
+import { WsDisconnectBtn } from "@/shared/test/wsDisconnectBtn";
 import { MainContent } from "@/shared/ui/mainContent";
 import { Sidebar } from "@/shared/ui/sidebar";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
-  const wsDisconnect = () => {
-    const socket = getSocket();
-    socket?.close();
-  };
   return (
     <>
       <Sidebar className="desktop:flex desktop:bg-main-light-gray bg-white">{children}</Sidebar>
 
       <MainContent className="desktop:flex hidden">
-        <Button className="m-3 w-50" size="md" onClick={wsDisconnect}>
-          Разорвать сокет
-        </Button>
+        <WsDisconnectBtn className="m-3 w-40" />
+        <QueryBtn className="m-3 w-50" />
       </MainContent>
     </>
   );
