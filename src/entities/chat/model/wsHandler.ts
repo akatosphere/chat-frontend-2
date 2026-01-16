5fba0b1d301ba8eb668b930d165ef66b9a988239
oseniy <ars7080947@gmail.com>
2026-01-16 16:30:53 +0300

    fix: исправил жалобу линтера

--- src/entities/chat/model/wsHandler.ts
+++ src/entities/chat/model/wsHandler.ts
@@ -4,9 +4,8 @@ export const chatWSHandler = (data: WSBaseResponse) => {
   const chatActions = ["create_chat"];
   if (!chatActions.includes(data.action)) return;
 
-  switch (
-    data.action
-    // здесь будет обработка приходящих ws сообщений, касающихся работы чата (новые сообщения, приглашения в группу и тд)
-  ) {
+  switch (data.action) {
+    case "example":
+      break;
   }
 };
