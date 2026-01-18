d7ada5aae4bbd6d5a5d54af27a8ffd0fbd8b4110
oseniy <ars7080947@gmail.com>
2026-01-19 02:14:38 +0300

    feat: редирект на страницу из контекстного меню

--- src/features/createGroupOrChannel/ui/createGroupBtn.tsx
+++ src/features/createGroupOrChannel/ui/createGroupBtn.tsx
@@ -1,19 +1,17 @@
-import Link from "next/link";
-
 import { cn } from "@/shared/shadcn/lib/utils";
 import { Button } from "@/shared/shadcn/ui/button";
-import CreateGroup from "@/shared/ui/icons/createGroup.svg";
+import CreateBtn from "@/shared/ui/icons/createGroupOrChannel/createBtn.svg";
+import { useCreateGrOrChContextMenu } from "@/widgets/contextMenu/lib/useCreateGrOrChContextMenu";
 
 type CreateGroupBtnProps = {
   className?: string;
 };
 
 export const CreateGroupBtn: React.FC<CreateGroupBtnProps> = ({ className }) => {
+  const { onContextMenu } = useCreateGrOrChContextMenu();
   return (
-    <Button asChild variant="ghost" size="icon-auto" className={cn(className)}>
-      <Link href="/create-group">
-        <CreateGroup className="text-primary h-6 w-6" />
-      </Link>
+    <Button variant="ghost" size="icon-auto" className={cn(className)} onClick={onContextMenu}>
+      <CreateBtn className="text-primary h-6 w-6" />
     </Button>
   );
 };
