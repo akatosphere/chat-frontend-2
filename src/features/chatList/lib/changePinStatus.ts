7f40b23d44a254deca3443ac0da41cbe6e855bfc
oseniy <ars7080947@gmail.com>
2026-01-19 01:02:36 +0300

    fix: убрал жалобу на отсутствие моковых данных

--- src/features/chatList/lib/changePinStatus.ts
+++ src/features/chatList/lib/changePinStatus.ts
@@ -1,6 +1,7 @@
-import { mockChats } from "./data";
-
-export const changePinStatus = async (chatId: string, pin: boolean) => {
-  const mock = mockChats.results.find((c) => c.id === +chatId)!;
-  if (mock) mock.is_favorite = pin;
-};
+export const changePinStatus = async () =>
+  // chatId: string, pin: boolean
+  {
+    // const mock = mockChats.results.find((c) => c.id === +chatId)!;
+    // if (mock) mock.is_favorite = pin;
+    console.log("changePinStatus");
+  };
