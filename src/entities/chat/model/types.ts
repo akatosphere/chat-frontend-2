a9e9093c671747e2752b1d055d1a74327b500584
oseniy <ars7080947@gmail.com>
2026-01-24 20:05:49 +0300

    feat: главный ридмик и начало сбора всех типов

--- src/entities/chat/model/types.ts
+++ src/entities/chat/model/types.ts
@@ -16,6 +16,24 @@ export interface LastMessage {
   updated_at: number;
 }
 
+export type ChatObject = {
+  chat_id: string;
+  chat_key: string;
+  name: string;
+  description: string;
+  chat_type: string;
+  created_by: string;
+  owner_full_name: string;
+  avatar: {
+    filename: string;
+    url: string;
+  } | null;
+  added_users: Array<{
+    uid: string;
+    full_name: string;
+  }>;
+};
+
 export interface ChatUser {
   uid: string;
   username: string;
