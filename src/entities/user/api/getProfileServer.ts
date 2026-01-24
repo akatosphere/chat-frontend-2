eed918a993dac3191786c7c1171c444518ef14e7
oseniy <ars7080947@gmail.com>
2026-01-24 23:27:44 +0300

    исправил импорты маппера

--- src/entities/user/api/getProfileServer.ts
+++ src/entities/user/api/getProfileServer.ts
@@ -1,14 +1,14 @@
 import { getApiServer } from "@/shared/api/getApiServer";
 import { Result } from "@/shared/api/types";
 
-import { mapUserDtoToUser } from "../model/mapper";
+import { mapUser } from "../model/mapper";
 import { User, UserDto } from "../model/types";
 
 export const getProfileServer = async (): Promise<Result<User>> => {
   try {
     const api = await getApiServer();
     const { data } = await api.post<UserDto>("/api/v1/auth/messenger/profile/");
-    return { success: true, data: mapUserDtoToUser(data) };
+    return { success: true, data: mapUser(data) };
   } catch {
     return { success: false, error: "Ошибка загрузки профиля на сервере" };
   }
