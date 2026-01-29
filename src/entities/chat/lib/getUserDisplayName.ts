26a9973e1a1dd2702c64bedda5f9190ffe177af6
TobyThorneA <igraman_XX@mail.ru>
2026-01-29 12:39:18 +0300

    поменял функцию getChatDisplayName

--- src/entities/chat/lib/getUserDisplayName.ts
+++ src/entities/chat/lib/getUserDisplayName.ts
@@ -1,7 +1,20 @@
 import { ChatItemData } from "../model/types";
 
-export const getChatDisplayName = (chat: ChatItemData) => {
-  const user = chat.chat;
+// export const getChatDisplayName = (chat: ChatItemData) => {
+//   const user = chat.chat;
 
-  return chat.name || `${user.first_name} ${user.last_name}`.trim() || user.username;
+//   return chat.name || `${user?.first_name} ${user?.last_name}`.trim() || user?.username;
+// };
+
+export const getChatDisplayName = (chat: ChatItemData): string => {
+  if (chat.name) {
+    return chat.name;
+  }
+
+  const user = chat.chat;
+  if (!user) {
+    return "Чат";
+  }
+
+  return `${user.first_name ?? ""} ${user.last_name ?? ""}`.trim() || user.username || "Чат";
 };
