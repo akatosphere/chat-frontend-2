d687c1124c76e77bbc9a96102f28895077bd5980
oseniy <ars7080947@gmail.com>
2026-01-30 20:55:53 +0300

    само починилось страница чатов

--- app/(chat)/@sidebar/default.tsx
+++ app/(chat)/@sidebar/default.tsx
@@ -1,9 +1,3 @@
-import { cn } from "@/shared/shadcn/lib/utils";
-
-type DefaultProps = {
-  className?: string;
-};
-
-export const Default: React.FC<DefaultProps> = ({ className }) => {
-  return <div className={cn("", className)}>ниче нету</div>;
-};
+export default function Default() {
+  return null; // или какой-то плейсхолдер
+}
