9aa801143b28d07354fda437e601ad4244bb005f
oseniy <ars7080947@gmail.com>
2026-02-17 18:47:50 +0300

    feat: обрезка текста для ссылки

--- src/shared/ui/infoItems/infoItem.tsx
+++ src/shared/ui/infoItems/infoItem.tsx
@@ -20,16 +20,18 @@ export const InfoItem: React.FC<InfoItemProps> = ({
   copy = false,
 }) => {
   return (
-    <div className="border-muted flex w-full cursor-default flex-wrap justify-between gap-2 overflow-hidden border-b px-3 py-2 last:border-0">
+    <div className="border-muted flex w-full cursor-default gap-2 overflow-hidden border-b px-3 py-2 last:border-0">
       <div className="flex min-w-0 flex-1 flex-col gap-1">
         {title && <p className="text-gray caption">{title}</p>}
         {text && (
           <div className={cn("flex min-w-0 items-center gap-2")}>
-            <p className={cn("subtext wrap-break-word", className)}>{text}</p>
+            <p className={cn(copy ? "subtext truncate" : "subtext wrap-break-word", className)}>
+              {text}
+            </p>
           </div>
         )}
       </div>
-      {copy && <CopyBtn text={textToCopy} className="shrink-0" />}
+      {copy && <CopyBtn text={textToCopy} className="shrink-0 self-center" />}
     </div>
   );
 };
