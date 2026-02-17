7e17597f12b0945230a49f8b87e44d1c8773fb37
oseniy <ars7080947@gmail.com>
2026-02-17 22:34:49 +0300

    нижний бордер для табсов

--- src/shared/shadcn/ui/tabs.tsx
+++ src/shared/shadcn/ui/tabs.tsx
@@ -55,10 +55,10 @@ function TabsTrigger({ className, ...props }: React.ComponentProps<typeof TabsPr
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring hover:text-primary subtext relative inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 border border-transparent px-2 py-1 font-medium whitespace-nowrap text-black transition-all group-data-[orientation=vertical]/tabs:w-full group-data-[orientation=vertical]/tabs:justify-start focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 group-data-[variant=default]/tabs-list:data-[state=active]:shadow-sm group-data-[variant=line]/tabs-list:data-[state=active]:shadow-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
-        "group-data-[variant=line]/tabs-list:data-[state=active]:border-b-primary border-4 group-data-[variant=line]/tabs-list:bg-transparent",
+        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring hover:text-primary subtext relative inline-flex h-9 flex-1 items-center justify-center gap-1.5 border border-transparent px-2 py-1 font-medium whitespace-nowrap text-black transition-all group-data-[orientation=vertical]/tabs:w-full group-data-[orientation=vertical]/tabs:justify-start focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 group-data-[variant=default]/tabs-list:data-[state=active]:shadow-sm group-data-[variant=line]/tabs-list:data-[state=active]:shadow-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "group-data-[variant=line]/tabs-list:bg-transparent",
         "data-[state=active]:bg-background data-[state=active]:text-primary",
-        "after:bg-foreground after:absolute after:opacity-0 after:transition-opacity group-data-[orientation=horizontal]/tabs:after:inset-x-0 group-data-[orientation=horizontal]/tabs:after:bottom-[-5px] group-data-[orientation=horizontal]/tabs:after:h-0.5 group-data-[orientation=vertical]/tabs:after:inset-y-0 group-data-[orientation=vertical]/tabs:after:-right-1 group-data-[orientation=vertical]/tabs:after:w-0.5 group-data-[variant=line]/tabs-list:data-[state=active]:after:opacity-100",
+        "after:bg-primary after:absolute after:inset-x-0 after:bottom-0 after:h-1 after:scale-x-0 after:rounded-[9999px_9999px_0_0] after:transition-transform group-data-[variant=line]/tabs-list:data-[state=active]:after:scale-x-100",
         className,
       )}
       {...props}
