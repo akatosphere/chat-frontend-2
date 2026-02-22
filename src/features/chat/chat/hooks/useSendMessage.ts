8c984d2c01b5d173ebdf6839da9b1885d8169fbf
lilsantt <brusnicagv@gmail.com>
2026-02-22 18:04:31 +0300

    feat: добавил возможность отправления видео

--- src/features/chat/chat/hooks/useSendMessage.ts
+++ src/features/chat/chat/hooks/useSendMessage.ts
@@ -2,12 +2,12 @@ import { useCallback } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import { sendTextMessage } from "@/entities/chat/api/sendMessage";
-import { PendingImage } from "@/features/chat/chat/model/store/useChatSendImagesStore";
 import { optimisticSendMessage } from "@/features/chatList/lib/optimisticSendMessage";
 import { MESSAGE_STATUS } from "@/shared/constants/constants";
 
 import { mapChatMessage } from "../model/mapper";
 import { buildMessageBlocks } from "../model/messageBlock/buildMessageBlocks";
+import { PendingMedia } from "../model/store/useChatSendImagesStore";
 import { useChatStore } from "../model/store/useChatStore";
 import { MappedChatMessage } from "../model/types/mappedTypes";
 import { ChatType } from "../model/types/serverTypes";
@@ -27,7 +27,7 @@ export const useSendMessage = () => {
   } = useChatStore();
 
   return useCallback(
-    async (text: string, images: PendingImage[] = []) => {
+    async (text: string, images: PendingMedia[] = []) => {
       // if (!text.trim() && images.length === 0) return; // не отправляем пустое
       if (!currentUserId || !chatKey) return;
       console.log(images);
--- src/widgets/contextMenu/lib/useSendFilesContextMenu.ts
+++ src/widgets/contextMenu/lib/useSendFilesContextMenu.ts
@@ -2,9 +2,9 @@ import { MouseEvent } from "react";
 
 import { useModalStore } from "@/entities/modals/model/useGlobalModalStore";
 import { openFilePicker } from "@/features/chat/chat/lib/openFilePicker";
-import { openImagePicker } from "@/features/chat/chat/lib/openImagePicker";
+import { openMediaPicker } from "@/features/chat/chat/lib/openImagePicker";
 import { useSendFilesStore } from "@/features/chat/chat/model/store/useChatSendFilesStore";
-import { useSendImageStore } from "@/features/chat/chat/model/store/useChatSendImagesStore";
+import { useSendMediaStore } from "@/features/chat/chat/model/store/useChatSendImagesStore";
 import File from "@/shared/ui/icons/sendFiles/file.svg";
 import Image from "@/shared/ui/icons/sendFiles/image.svg";
 
@@ -12,7 +12,7 @@ import { useContextMenu } from "../ui/contextMenuProvider";
 
 export const useSendFilesContextMenu = () => {
   const { openMenu, activeMenuId } = useContextMenu();
-  const addImages = useSendImageStore((s) => s.addImages);
+  const addImages = useSendMediaStore((s) => s.addMedia);
   const addFiles = useSendFilesStore((s) => s.addFiles);
   const openModal = useModalStore((s) => s.openModal);
   const menuId = "sendFiles";
@@ -27,7 +27,7 @@ export const useSendFilesContextMenu = () => {
             label: "Выбрать изображение",
             icon: Image,
             onClick: async () => {
-              const files = await openImagePicker();
+              const files = await openMediaPicker();
               addImages(files);
               openModal("sendImage", { chatKey: "" });
             },
