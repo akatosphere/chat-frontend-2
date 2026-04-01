fab6a6230f480f301548d356aa10e41976bcf18b
oseniy <ars7080947@gmail.com>
2026-04-01 13:25:02 +0300

    fix: метод reset стора useChatStore теперь очищает еще и медиа

--- src/entities/chat/model/useChatStore.ts
+++ src/entities/chat/model/useChatStore.ts
@@ -231,6 +231,9 @@ export const useChatStore = create<ChatState>((set) => ({
       chatKey: null,
       isReady: false,
       replyTarget: null,
+      media: [],
+      isLoadingMedia: false,
+      isMediaLoaded: false,
       // forwardTargets: [],
     }),
 }));
